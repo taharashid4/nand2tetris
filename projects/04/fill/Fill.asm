3fecb614faa096174db269ee7eabcf4cf088ec5a
harthedir <114638313+harthedir@users.noreply.github.com>
2023-03-11 22:41:34 +0500

    Add files via upload

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,87 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+
+// Storing 16384 (base location of the memory map) in address to begin every iteration
+
+@SCREEN
+D=A
+@address
+M=D
+
+@KBD
+D=M
+@BLACKLOOP
+
+// If the user inputs anything, contents of KBD != 0, -> make the screen black
+
+D;JNE
+
+// If no input, repeatedly make the screen white (not very efficient :()
+
+@WHITELOOP
+0;JMP
+
+(BLACKLOOP)
+
+// The logic to iterate over every register as address is incremented
+
+@address
+D=M
+@SCREEN
+D=D-A
+@SCREEN
+A=A+D
+
+// -1 in 2s complement is all 1s -> black screen
+
+M=-1
+
+// Incrementing the value of address to fill the next register
+
+@address
+M=M+1
+
+// If the value stored in address is the same as the final screen index (== KBD), repeat from the start.
+
+@KBD
+D=A
+@address
+D=D-M
+
+// If we haven't arrived at the final memory map location, continue filling the screen.
+
+@BLACKLOOP
+D;JNE
+
+@LOOP
+0;JMP
+
+(WHITELOOP)
+
+// The exact same logic as used above, but this time for white pixels (M == 0)
+
+@address
+D=M
+@SCREEN
+D=D-A
+@SCREEN
+A=A+D
+
+M=0
+
+@address
+M=M+1
+
+@KBD
+D=A
+@address
+D=D-M
+
+@WHITELOOP
+D;JNE
+
+@LOOP
+0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,68 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+
+@i
+M=0
+
+@R2
+M=0
+
+// sum stores the repeated addition of the number
+@sum
+M=0
+
+(LOOP)
+
+// If i == R1 (the number of times the number needs to be added to itself), break
+@R1
+D=M
+@i
+D=D-M
+@END
+D;JEQ
+
+// If R0 = 0, must break
+@R0
+D=M
+@END
+D;JEQ
+
+// If R1 = 0, break
+@R1
+D=M
+@END
+D;JEQ
+
+@R0
+D=M
+@sum
+M=M+D
+@i
+M=M+1
+
+@LOOP
+0;JMP
+
+(END)
+// If i == 0, RAM[R2] must be 0
+@i
+D=M
+@SETZERO
+D;JEQ
+
+@sum
+D=M
+@R2
+M=D
+
+(REPEAT)
+@REPEAT
+0;JMP
+
+(SETZERO)
+@R2
+M=0
+@REPEAT
+0;JMP
